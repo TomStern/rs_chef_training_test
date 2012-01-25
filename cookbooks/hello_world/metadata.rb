72702cea7ad8f462ae3d3824d8d43ae10c119b98
TomStern <thomas@rightscale.com>
2012-01-25 22:07:12 +0000

    Added the hello_world cookbook

---
+++ cookbooks/hello_world/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RIGHTSCALE, Inc."
+maintainer_email "thomas@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures hello_world"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "hello_world::default", "Prints to the audit entry -- tas"
---
+++ cookbooks/hello_world/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: hello_world
+# Recipe:: default
+#
+# Copyright 2012, RightScale <thomas@rightscale.com>
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World!"
+log "Success!  Tom..."
